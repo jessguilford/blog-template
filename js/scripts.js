72f0886f4d1b65b0c8041a9b69d84ffaf477bcb8
Jessie Guilford <Jessie Guilford>
2016-02-26 16:39:16 -0800

    gh pages

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".year").click(function(event) {
+    $("#months-" + this.id).slideToggle();
+    $(".month-posts").hide();
+  });
+  $(".month").click(function(event) {
+    $(this).next(".month-posts").slideToggle();
+  });
+});
